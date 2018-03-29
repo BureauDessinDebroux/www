376c02135ee2ce6f15cde07d52f3df91f94e4715
Alexis <awo@opinum.com>
2018-03-29 23:11:05 +0200

    still header

--- dist/944be636eb7d10e56caa03a6ea64509c.js
+++ dist/944be636eb7d10e56caa03a6ea64509c.js
@@ -71,7 +71,7 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({12:[function(require,module,exports) {
+})({11:[function(require,module,exports) {
 var bundleURL = null;
 function getBundleURLCached() {
   if (!bundleURL) {
@@ -101,7 +101,7 @@ function getBaseURL(url) {
 
 exports.getBundleURL = getBundleURLCached;
 exports.getBaseURL = getBaseURL;
-},{}],9:[function(require,module,exports) {
+},{}],8:[function(require,module,exports) {
 var bundle = require('./bundle-url');
 
 function updateLink(link) {
@@ -132,13 +132,13 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":12}],8:[function(require,module,exports) {
+},{"./bundle-url":11}],5:[function(require,module,exports) {
 
         var reloadCSS = require('_css_loader');
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./..\\images\\arrow--right.svg":[["cc03c1e82bae429d5482d6f6c01c2a08.svg",53],53],"./..\\images\\arrow--down.svg":[["64220bcebd7231871456056f85bf9e67.svg",54],54],"_css_loader":9}],35:[function(require,module,exports) {
+},{"./..\\images\\arrow--right.svg":[["cc03c1e82bae429d5482d6f6c01c2a08.svg",9],9],"./..\\images\\arrow--down.svg":[["64220bcebd7231871456056f85bf9e67.svg",10],10],"_css_loader":8}],7:[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -155,7 +155,7 @@ function mobilecheck() {
 
     return check;
 };
-},{}],34:[function(require,module,exports) {
+},{}],6:[function(require,module,exports) {
 'use strict';
 
 var _mobilecheck = require('./mobilecheck.js');
@@ -177,7 +177,7 @@ var mobileHeaderHeight = function mobileHeaderHeight() {
 };
 
 mobileHeaderHeight();
-},{"./mobilecheck.js":35}],59:[function(require,module,exports) {
+},{"./mobilecheck.js":7}],4:[function(require,module,exports) {
 var mobileNavTrigger = function () {
     var trigger = document.querySelector('.nav__trigger'),
         nav = document.querySelector('.nav'),
@@ -193,7 +193,7 @@ var mobileNavTrigger = function () {
         html.classList.contains('no-scroll') ? html.classList.remove('no-scroll') : html.classList.add('no-scroll');
     });
 }();
-},{}],6:[function(require,module,exports) {
+},{}],2:[function(require,module,exports) {
 "use strict";
 
 require("./assets/sass/app.scss");
@@ -201,7 +201,7 @@ require("./assets/sass/app.scss");
 require("./assets/js/header.js");
 
 require("./assets/js/mobilenav.js");
-},{"./assets/sass/app.scss":8,"./assets/js/header.js":34,"./assets/js/mobilenav.js":59}],106:[function(require,module,exports) {
+},{"./assets/sass/app.scss":5,"./assets/js/header.js":6,"./assets/js/mobilenav.js":4}],90:[function(require,module,exports) {
 
 var global = (1, eval)('this');
 var OldModule = module.bundle.Module;
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '58245' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51421' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -324,5 +324,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.require, id);
   });
 }
-},{}]},{},[106,6])
+},{}]},{},[90,2])
 //# sourceMappingURL=/dist/944be636eb7d10e56caa03a6ea64509c.map
