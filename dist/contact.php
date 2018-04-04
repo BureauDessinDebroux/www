436963a1f0c74288df405d83254f9ad029fa85c8
Alexis <awo@opinum.com>
2018-04-04 20:41:00 +0200

    Contact form test

--- dist/contact.php
+++ dist/contact.php
@@ -2,12 +2,6 @@
 
 echo '1';
 
-$name       = $_POST['name']; 
-$mail       = $_POST['mail']; 
-$message    = $_POST['message'];
-
-echo $name;
-echo $mail;
-echo $message;
+echo $_POST['name'];
 
 ?>
