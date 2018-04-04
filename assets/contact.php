1d8cde6c05d3625e05ab7081efeb02e200df29b3
Alexis <awo@opinum.com>
2018-04-04 22:04:11 +0200

    Contact form test

--- assets/contact.php
+++ assets/contact.php
@@ -5,36 +5,45 @@ if(isset($_POST['name'])) {
     // define variables and set to empty values
     $ERR_NAME = $ERR_EMAIL = $ERR_MESSAGE = "";
     $NAME = $EMAIL = $MESSAGE = "";
+    $honeypot = false;
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        if(empty($_POST["name"])) {
-            $ERR_NAME = "Manque le nom";
-        } else {
-            $NAME = test_input($_POST["name"]);
-        }
-        
-        if(empty($_POST["name"])) {
-            $ERR_MAIL = "Manque le mail";
-        } else {
-            $EMAIL = test_input($_POST["email"]);
-        }
-        
-        if(empty($_POST["name"])) {
-            $ERR_MESSAGE = "Manque le message";
-        } else {
-            $MESSAGE = test_input($_POST["message"]);
-        }
+        if(!empty($_REQUEST['company']) && (bool) $_REQUEST['company'] == true) {
+            
+            $honeypot = true;
+            log_spambot($_REQUEST);
 
-        // CONFIGURATION
-        $RECIEVER   = "wollsale@gmail.com";
-        $FROM       = "From: $NAME <$EMAIL>\r\nReturn-path: $EMAIL";
-        $SUBJECT    = "[bureaudessindebroux.be] Message reçu de $NAME ($EMAIL)";
+        } else {
 
-        // MAIL SERVICE
-        mail($RECIEVER, $SUBJECT, $MESSAGE, $FROM);
-
-        header('Location: index.html');
+            if(empty($_POST["name"])) {
+                $ERR_NAME = "Manque le nom";
+            } else {
+                $NAME = test_input($_POST["name"]);
+            }
+            if(empty($_POST["name"])) {
+                $ERR_MAIL = "Manque le mail";
+            } else {
+                $EMAIL = test_input($_POST["email"]);
+            }
+            if(empty($_POST["name"])) {
+                $ERR_MESSAGE = "Manque le message";
+            } else {
+                $MESSAGE = test_input($_POST["message"]);
+            }
+    
+            // CONFIGURATION
+            $RECIEVER   = "wollsale@gmail.com";
+            $FROM       = "From: $NAME <$EMAIL>\r\nReturn-path: $EMAIL";
+            $SUBJECT    = "[bureaudessindebroux.be] Message reçu de $NAME ($EMAIL)";
+    
+            // MAIL SERVICE
+            mail($RECIEVER, $SUBJECT, $MESSAGE, $FROM);
+            
+            // REDIRECTION
+            header('Location: index.html');
+            
+        }
         
     }
 }
