9c9232a388aef528b89d8948a38a0d02ffc8cfe7
Alexis <awo@opinum.com>
2018-04-04 20:39:00 +0200

    Contact form test

--- assets/contact.php
+++ assets/contact.php
@@ -1,15 +1,11 @@
 <?php 
 
-    if(isset($_POST['name'])) {
+$name       = $_POST['name']; 
+$mail       = $_POST['mail']; 
+$message    = $_POST['message'];
 
-        $name       = $_POST['name']; 
-        $mail       = $_POST['mail']; 
-        $message    = $_POST['message'];
-
-        echo $name;
-        echo $mail;
-        echo $message;
-
-    }
+echo $name;
+echo $mail;
+echo $message;
 
 ?>
