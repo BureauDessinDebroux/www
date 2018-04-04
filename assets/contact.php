7bb77c912745fa6093f1c02acf08786415d44005
Alexis <awo@opinum.com>
2018-04-04 20:53:30 +0200

    Contact form test

--- assets/contact.php
+++ assets/contact.php
@@ -5,9 +5,18 @@ echo '2';
 if(isset($_POST['name'])) {
 
     $NAME       = $_POST['name'];
+    $EMAIL      = $_POST['mail'];
+    $MESSAGE    = $_POST['message'];
 
-    var_dump($NAME);
 
+    // CONFIGURATION
+    $RECIEVER   = "wollsale@gmail.com";
+    $FROM       = "From: $NAME <$EMAIL>\r\nReturn-path: $EMAIL";
+    $SUBJECT    = "Nouveau message reçu de $NAME";
+
+    mail($RECIEVER, $SUBJECT, $MESSAGE, $FROM);
+
+    echo 'sent';
 }
 
 ?>
