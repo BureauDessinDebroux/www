490d668fe91b65f058210b90b40a25bd11fccc7d
Alexis <awo@opinum.com>
2018-05-02 13:20:31 +0200

    Improve highlight Chrome

--- assets/js/nav/smoothScroll.js
+++ assets/js/nav/smoothScroll.js
@@ -47,7 +47,7 @@ export function scrollIt(destination, duration = 200, easing = 'linear', callbac
     const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
     const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
     const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
+    const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset + 20);
 
     if ('requestAnimationFrame' in window === false) {
         window.scroll(0, destinationOffsetToScroll);
