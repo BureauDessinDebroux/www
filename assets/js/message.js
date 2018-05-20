b779194947a3470a79d070f2721ff9682df8dd7d
Alexis Wollseifen <alexis@MBP-de-Alexis.lan>
2018-05-20 17:43:52 +0200

    Contact checkbox

--- assets/js/message.js
+++ assets/js/message.js
@@ -7,11 +7,16 @@ const messageFeedback = function() {
     let MESSAGE = getURLParameter('message'),
         OUTTER  = document.querySelector('.form__input--submit'),
         OUTTER_VALUE = 'message envoyé',
+        LEGAL_ERROR = 'Veuillez lire et acceptez les mentions légales avant d\'envoyer votre message',
         FEEDBACK = document.querySelector('.contact__feedback');
             
     if(MESSAGE == 'message sent') {
         OUTTER.classList.add('form__input--submitted');
         OUTTER.value = OUTTER_VALUE;
+        FEEDBACK.classList.add('contact__feedback--is-visible');
+    }
+    if(MESSAGE == 'legal error') {
+        FEEDBACK.innerHTML = LEGAL_ERROR;
         FEEDBACK.classList.add('contact__feedback--is-visible');
     }
 
